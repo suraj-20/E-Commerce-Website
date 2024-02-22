03e04cb9ea20c0ba778770d48cc818bf70570a8c
Suraj <kumarsuraj54057@gmail.com>
2024-02-22 23:08:49 +0530

    multer

--- E-Commerce Web/backend/index.js
+++ E-Commerce Web/backend/index.js
@@ -1,1 +1,43 @@
+require("dotenv").config();
+const path = require("path");
 const express = require("express");
+const { connectMognoDb } = require("./config/mongodb");
+const cors = require("cors");
+const multer = require("multer");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+connectMognoDb(process.env.MONGO_URL);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static(path.resolve("uploads/images")));
+
+app.get("/", (req, res) => {
+  res.send("Hello Devloper");
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, path.resolve(`./uploads/images`));
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${path.extname(file.originalname)}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port:${PORT}`);
+});
